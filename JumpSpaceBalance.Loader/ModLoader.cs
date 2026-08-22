4d7ca687101a8087c8a41fe5fd0166e7cfaad0b2
gurudennis <gurudenis@gmail.com>
2026-08-22 17:20:32 -0400

    Initial complete loader

--- JumpSpaceBalance.Loader/ModLoader.cs
+++ JumpSpaceBalance.Loader/ModLoader.cs
@@ -1,9 +1,32 @@
-﻿using MelonLoader;
-
-namespace JumpSpaceBalance.Loader
+﻿namespace JumpSpaceBalance.Loader
 {
-    public class ModLoader : MelonMod
+    public class ModLoader : MelonLoader.MelonMod
     {
+        public override void OnInitializeMelon()
+        {
+            string path = Path.Combine(MelonLoader.Utils.MelonEnvironment.UserLibsDirectory, "JumpSpaceBalance.dll");
+            dll_ = MelonLoader.NativeLibrary.LoadLib(path);
+        }
 
+        public override void OnDeinitializeMelon()
+        {
+            try
+            {
+                if (dll_ != IntPtr.Zero)
+                {
+                    IntPtr symbolAddress = MelonLoader.NativeLibrary.GetExport(dll_, "JumpSpaceBalance_Finalize");
+                    if (symbolAddress != IntPtr.Zero)
+                    {
+                        Action finalize = System.Runtime.InteropServices.Marshal.GetDelegateForFunctionPointer<Action>(symbolAddress);
+                        if (finalize != null)
+                        {
+                            finalize();
+                        }
+                    }
+                }
+            } catch { }
+        }
+
+        private IntPtr dll_;
     }
 }
