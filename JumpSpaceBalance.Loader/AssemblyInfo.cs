ee09376b81aaf1fd241ae8ebe1202c91c4672ace
gurudennis <gurudenis@gmail.com>
2026-08-23 13:30:31 -0400

    Version info

--- JumpSpaceBalance.Loader/AssemblyInfo.cs
+++ JumpSpaceBalance.Loader/AssemblyInfo.cs
@@ -1,19 +1,21 @@
-using MelonLoader;
-using System.Runtime.InteropServices;
 using JumpSpaceBalance.Loader;
-
-// In SDK-style projects such as this one, several assembly attributes that were historically
-// defined in this file are now automatically added during build and populated with
-// values defined in project properties. For details of which attributes are included
-// and how to customise this process see: https://aka.ms/assembly-info-properties
-
-
-// Setting ComVisible to false makes the types in this assembly not visible to COM
-// components.  If you need to access a type in this assembly from COM, set the ComVisible
-// attribute to true on that type.
+using MelonLoader;
+using System.Reflection;
+using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 [assembly: Guid("aba36fec-92d8-44f4-babe-4fa863cbc9cd")]
 
-[assembly: MelonInfo(typeof(ModLoader), "JumpSpaceBalance", "1.0.0.0", "gurudennis", "https://github.com/gurudennis/JumpSpaceBalance/releases")]
+[assembly: AssemblyTitle("JumpSpaceBalance")]
+[assembly: AssemblyDescription("Balance mod for Jump Space")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("JumpSpaceBalance")]
+[assembly: AssemblyCopyright("Copyright © gurudennis 2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
+
+[assembly: MelonInfo(typeof(ModLoader), "JumpSpaceBalance", "1.0.1.0", "gurudennis", "https://github.com/gurudennis/JumpSpaceBalance/releases")]
 [assembly: MelonGame("Keepsake Games", "Jump Space")]
